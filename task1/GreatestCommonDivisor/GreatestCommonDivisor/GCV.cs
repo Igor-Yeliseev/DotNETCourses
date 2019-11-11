b0533b184dc9189da95cb85cf8b222f4cc5d610e
Igor Yelis <yeliswiseguy@gmail.com>
2019-11-11 22:06:38 +0300

    Добавлены перегрузки для 3, 4, 5 чисел и тесты к ним.

--- task1/GreatestCommonDivisor/GreatestCommonDivisor/GCV.cs
+++ task1/GreatestCommonDivisor/GreatestCommonDivisor/GCV.cs
@@ -37,7 +37,7 @@ namespace GreatestCommonDivisor
         }
 
         /// <summary>
-        /// Алгоритм Евклида для нахождения НОД трех чисел
+        /// Алгоритм для нахождения НОД трех чисел
         /// </summary>
         /// <param name="a"> Первое число</param>
         /// <param name="b"> Второе число</param>
@@ -45,8 +45,37 @@ namespace GreatestCommonDivisor
         /// <returns></returns>
         public static int EuclideanGCV(int a, int b, int c)
         {
-            return 0;
+            return EuclideanGCV(EuclideanGCV(a, b), c);
         }
+
+        /// <summary>
+        /// Алгоритм для нахождения НОД четырех чисел
+        /// </summary>
+        /// <param name="a"> Первое число</param>
+        /// <param name="b"> Второе число</param>
+        /// <param name="c"> Третье число</param>
+        /// <param name="d"> Четвертое число</param>
+        /// <returns></returns>
+        public static int EuclideanGCV(int a, int b, int c, int d)
+        {
+            return EuclideanGCV(EuclideanGCV(EuclideanGCV(a, b), c), d);
+        }
+
+        /// <summary>
+        /// Алгоритм для нахождения НОД пяти чисел
+        /// </summary>
+        /// <param name="a"> Первое число</param>
+        /// <param name="b"> Второе число</param>
+        /// <param name="c"> Третье число</param>
+        /// <param name="d"> Четвертое число</param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        public static int EuclideanGCV(int a, int b, int c, int d, int e)
+        {
+            return EuclideanGCV(EuclideanGCV(EuclideanGCV(EuclideanGCV(a, b), c), d), e);
+        }
+
+
 
     }
 }
--- task1/UnitTestsGCD/UnitTest1.cs
+++ task1/UnitTestsGCD/UnitTest1.cs
@@ -53,8 +53,57 @@ namespace UnitTestsGCD
         }
 
 
+        [TestMethod]
+        public void Test4EuclideanGCV()
+        {
+            // arrange
+            int expected = 6;
+
+            // act
+            int a = 78;
+            int b = 294;
+            int c = 570;
+            int answer = GCV.EuclideanGCV(a, b, c);
+
+            // assert
+            Assert.AreEqual(expected, answer);
+        }
 
 
+        [TestMethod]
+        public void Test5EuclideanGCV()
+        {
+            // arrange
+            int expected = 6;
 
+            // act
+            int a = 78;
+            int b = 294;
+            int c = 570;
+            int d = 612;
+            int answer = GCV.EuclideanGCV(a, b, c, d);
+
+            // assert
+            Assert.AreEqual(expected, answer);
+        }
+
+
+        [TestMethod]
+        public void Test6EuclideanGCV()
+        {
+            // arrange
+            int expected = 23;
+
+            // act
+            int a = 161;
+            int b = 253;
+            int c = 230;
+            int d = 529;
+            int e = 437;
+            int answer = GCV.EuclideanGCV(a, b, c, d, e);
+
+            // assert
+            Assert.AreEqual(expected, answer);
+        }
     }
 }
