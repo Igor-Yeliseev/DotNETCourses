d2dc7c4964e3144d20aa467b5f6e31518a97f9ac
Igor Yelis <yeliswiseguy@gmail.com>
2019-11-18 22:34:35 +0300

    Начата разработка класса Polynom

---
+++ task2/Algebra/Algebra/Polynom.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algebra
+{
+    class Polynom
+    {
+        /// <summary>
+        /// Старшая степень полинома
+        /// </summary>
+        public int Degree { get; set; }
+        /// <summary>
+        /// Массив коэффициентов полинома при каждой степени
+        /// </summary>
+        private double[] coeffs;
+        
+
+        /// <summary>
+        /// Конструктор полинома
+        /// </summary>
+        /// <param name="coeffs"> Коэффициенты при степенях начиная со старшей</param>
+        public Polynom(params double[] coeffs)
+        {
+            Degree = coeffs.Length;
+            this.coeffs = new double[Degree];
+
+            for (int i = 0; i < coeffs.Length; i++)
+            {
+                this.coeffs[i] = coeffs[i];
+                
+            }
+        }
+
+    }
+}
