0c8da67a501ff37dee7ac2daef875a7dd7a07f0f
Igor Yelis <yeliswiseguy@gmail.com>
2019-12-28 18:41:55 +0300

    Добавлены типы узла дерева и самого дереве

---
+++ task5/BinaryTreeLibrary/BinaryTreeLibrary/Node.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeLibrary
+{
+    /// <summary>
+    /// A class representing a tree node
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Node<T> : IComparable
+        where T : IComparable
+    {
+
+        /// <summary>
+        /// Node data
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// Left node
+        /// </summary>
+        public Node<T> Left { get; set; }
+
+        /// <summary>
+        /// Right node
+        /// </summary>
+        public Node<T> Right { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the Node class
+        /// </summary>
+        /// <param name="data"> Node data</param>
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Node class
+        /// </summary>
+        /// <param name="data"> Node data</param>
+        /// <param name="left"> Left node</param>
+        /// <param name="right"> Right node</param>
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+
+        /// <summary>
+        /// Add a new element to node
+        /// </summary>
+        /// <param name="data"> Node data</param>
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+
+            if (node.Data.CompareTo(Data) == -1)
+            {
+                if (Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if(Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Compares the current instance with another object of the same type.
+        /// </summary>
+        /// <param name="obj"> An object to compare with this instance.</param>
+        /// <returns></returns>
+        public int CompareTo(object obj)
+        {
+            if(obj is Node<T> item)
+            {
+                return Data.CompareTo(item);
+            }
+            else
+            {
+                throw new InvalidCastException("Unable to cast types");
+            }
+        }
+    }
+}
---
+++ task5/BinaryTreeLibrary/BinaryTreeLibrary/Tree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeLibrary
+{
+    public class Tree<T>
+        where T : IComparable
+    {
+        /// <summary>
+        /// Tree root element
+        /// </summary>
+        public Node<T> Root { get; private set; }
+
+        /// <summary>
+        /// The number of tree elements
+        /// </summary>
+        public int Count { get; private set; }
+
+        public void Add(T data)
+        {
+            if(Root == null)
+            {
+                Root = new Node<T>(data);
+                Count = 1;
+                return;
+            }
+
+            Root.Add(data);
+            Count++;
+        }
+    }
+}
